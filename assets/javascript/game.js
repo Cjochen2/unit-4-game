e3a387d38d77d7085b2668265f2f6ff131d986dc
Curtis Jochen <curtis.jochen@gmail.com>
2019-06-09 15:23:54 -0400

    Added some Html and began structuring the page. Added some JS and created a function to generate the guess number

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+var targerNumber;
+var numberOptions;   
+
+
+function gameReset(){
+    targerNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19)
+    console.log(targerNumber);
+
+}
+gameReset();
