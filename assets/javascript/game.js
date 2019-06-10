19e5e74351b129ddb32412b1482ef30a737c81c7
Curtis Jochen <curtis.jochen@gmail.com>
2019-06-10 15:51:41 -0400

    Made the javascript so that the game is playable through one time. There is an issue on reset that needs to be addressed. Something to do with the images and re adding them with new values. CSS also needs to be done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,63 @@
-var targerNumber;
-var numberOptions;   
+var targetNumber;
+var numberOptions = [];
+var imageGenerator;
+var arrayGenerator;
+var picArray = ["assets/images/penguin.jpeg", "assets/images/bane.jpg", "assets/images/ivy.jpg", "assets/images/szasz.jpg"];
+var counter = 0;
+var wins = 0;
+var lose = 0;
 
-
-function gameReset(){
-    targerNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19)
-    console.log(targerNumber);
-
-}
 gameReset();
+function gameReset(){
+    counter = 0;
+    numberOptions =[];
+    $("#userNumber").empty();
+    $("#pictures").empty();
+
+    targetNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+    console.log(targetNumber);
+    $("#number-to-guess").text(targetNumber);
+    
+    while(numberOptions.length < 4 ){
+        arrayGenerator = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+        if(numberOptions.indexOf(arrayGenerator) < 0)
+        numberOptions.push(arrayGenerator);
+        console.log(numberOptions);
+    }
+
+    for(i = 0; i < numberOptions.length; i++){
+        imageGenerator = $("<img>");
+        
+        imageGenerator.addClass("image");
+
+        imageGenerator.attr("src", picArray[i]);
+
+        imageGenerator.attr("data-value", numberOptions[i]);
+
+        $("#pictures").append(imageGenerator);
+
+    };
+};
+
+
+$(".image").on("click", function() {
+    var imageValue = ($(this).attr("data-value"));
+
+    imageValue = parseInt(imageValue);
+
+    counter += imageValue;
+    console.log(counter);
+
+    $("#userNumber").text(counter);
+    if(counter === targetNumber){
+        wins++;
+        $("#wins").text(wins);
+        gameReset();
+    }
+
+    else if(counter > targetNumber){
+        lose++;
+        $("#losses").text(lose);
+        gameReset();
+    }
+});
